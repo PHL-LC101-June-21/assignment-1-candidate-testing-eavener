af04cf89b8e4d9e1276a0c84f36b3560bc925a1f
eavener <85445136+eavener@users.noreply.github.com>
2021-06-26 21:25:58 +0000

    Mostly done but need a few things checked.

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,49 +6,94 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = [
+"Who was the first American woman in space? ",
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "
+];
+let correctAnswers = ["Sally Ride",
+"true",
+"40",
+"Trajectory",
+"3"];
+let candidateAnswers = [];
+let candidateCorrectAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-let candidateName =  input.question("What is your name? ");
-console.log("Hello, " + candidateName + ".");
-
+candidateName =  input.question("What is your name? ");
+console.log("Hello, " + candidateName + ". Welcome to the initial screening test. Please remember that answers are case sensitive.");
+return candidateName
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-let candidateAnswer = input.question("Who was the first American woman in space? ");
-console.log("You answered " + candidateAnswer);
+candidateAnswers = input.question();
+return candidateAnswers;
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-let correctAnswer = ("Sally Ride");
-if (candidateAnswer === correctAnswer) {
-  console.log("Correct! Sally Ride was the first American woman in space.");
-} else {
-  console.log("Sorry, that is incorrect. Sally Ride was the first American woman in space.");
-}
-  let grade;
-  
+for (let i = 0; i < questions.length; i++) {
+  if (candidateAnswers[i] === correctAnswers[i]){
+    candidateCorrectAnswers.push(candidateAnswers)
+  }
 
-  return grade;
+}
+let grade = ((candidateCorrectAnswers.length) / (correctAnswers.length)) * 100;
+return grade;
 }
 
 function runProgram() {
-  askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
+askForName();
+for (let i = 0; i < questions.length; i++) {
+  candidateAnswers = askQuestion(questions[i]);
 }
+gradeQuiz(this.candidateAnswers);
+}
+
+runProgram;
+
+let candidateStatus = "";
+if (grade >= 80){
+  candidateStatus = "PASSED";
+  } else {
+    candidateStatus = "FAILED";
+  }
+
+console.log(
+"Candidate Name: " + candidateName "\n"
+"1) " + questions[1]\n
+"Your Answer: " + candidateAnswers[1]\n
+"Correct Answer: " + correctAnswers [1]\n
+\n
+"2) " + questions[2]\n
+"Your Answer: " + candidateAnswers[2]\n
+"Correct Answer: " + correctAnswers [2]\n
+\n
+"3) " + questions[3]\n
+"Your Answer: " + candidateAnswers[3]\n
+"Correct Answer: " + correctAnswers [3]\n
+\n
+"4) " + questions[4]\n
+"Your Answer: " + candidateAnswers[4]\n
+"Correct Answer: " + correctAnswers[4]"\n"
+\n
+"5) " + questions[5]\n
+"Your Answer: " + candidateAnswers[5]\n
+"Correct Answer: " + correctAnswers [5]\n
+\n
+">>> Overall Grade: " + grade + "% (" + (candidateCorrectAnswers.length) " of " + (correctAnswers.length) " responses correct) <<<"\n
+">>> Status: " + candidateStatus + " <<<)"
+)
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
